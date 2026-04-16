14e451b845b94400a1507675c2082f2b20bf3f29
Ziad Mohamed <146951921+ziad-mohamed-mern@users.noreply.github.com>
2026-04-16 14:12:07 +0200

    Update axios instance base URL

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://movies-website-production.up.railway.app",
+  baseURL: "https://movies-website-production-fd3c.up.railway.app/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
